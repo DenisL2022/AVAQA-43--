9b9014678d94e6995251d57b3a3037b00b78a597
DenisL2022 <109528523+DenisL2022@users.noreply.github.com>
2022-08-31 15:44:04 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5019;
+        int milesPrice = 20;
+        
+        int miles = price / milesPrice;
+        System.out.println(miles);
+    }
+}
